4498881898205c8b1fafd3610e28e5db3595b236
Aday Bujeda <aday@theagilemonkeys.com>
2025-07-01 11:18:06 +0100

    Improved message management for scripts

--- .github/scripts/utils.sh
+++ .github/scripts/utils.sh
@@ -2,6 +2,11 @@
 set_output() {
   local name="$1"
   local value="$2"
+  local fallback="${3:-_not provided_}"
+  if [[ -z "$value" ]]; then
+    value="$fallback"
+  fi
+
   echo "::notice title=Output::$name::${value}"
   echo "${name}<<EOF" >> "$GITHUB_OUTPUT"
   echo "$value" >> "$GITHUB_OUTPUT"
